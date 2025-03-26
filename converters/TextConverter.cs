5be545350b26fb22e9a7ea904c19125fae7283ba
KnuckleMonkey <jrodkey@gmail.com>
2025-03-25 19:02:20 -0500

    Paragraphs are working along more of refactoring. Next up, paragraphs need to allow the headers to go in by themselves and I need to get the list to work.

--- converters/TextConverter.cs
+++ converters/TextConverter.cs
@@ -1,16 +1,13 @@
 using System.Text;
+using System.Text.RegularExpressions;
 using ConvertMarkdownToHTML.conversion;
 
 namespace ConvertMarkdownToHTML.converters
 {
     public abstract class TextConverter : IConverter<string, TokenConversion>
     {
-        protected Dictionary<string, TokenConversion> m_conversions;
-
-        public TextConverter()
-        {
-            m_conversions = GetConversions();
-        }
+        protected string m_supportedConversions = "";
+        protected Dictionary<string, TokenConversion> m_conversions = new Dictionary<string, TokenConversion>();
 
         /// <summary>
         /// 
@@ -46,6 +43,15 @@ namespace ConvertMarkdownToHTML.converters
         /// <summary>
         /// 
         /// </summary>
+        public virtual string SupportedConversion
+        {
+            get { return m_supportedConversions; }
+            set { m_supportedConversions = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="text"></param>
         /// <param name="index"></param>
         /// <returns></returns>
@@ -54,14 +60,34 @@ namespace ConvertMarkdownToHTML.converters
             int incrementIndex = index + 1;
             StringBuilder sb = new StringBuilder();
             sb.Append(text[index]);
-            while ((text[incrementIndex] == text[index]) && (incrementIndex < text.Length))
+            while ((incrementIndex < text.Length) && (text[incrementIndex] == text[index]))
             {
                 sb.Append(text[incrementIndex++]);
             }
 
             return sb.ToString();
         }
-        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public bool IsValidMatch(char character)
+        {
+            return Regex.Match(character.ToString(), SupportedConversion).Success;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public bool IsValidMatch(char character, string pattern)
+        {
+            return Regex.Match(character.ToString(), pattern).Success;
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -69,12 +95,44 @@ namespace ConvertMarkdownToHTML.converters
         /// <returns></returns>
         public string Replace(string name)
         {
-            if (!m_conversions.ContainsKey(name))
+            return (!m_conversions.ContainsKey(name)) ? string.Empty : m_conversions[name].Get();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public (string, int) GetTextReplacement(int index, ReadOnlySpan<char> text, TokenConversion? conversion = null)
+        {
+            return GetTextReplacement(index, text, conversion, SupportedConversion);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public (string, int) GetTextReplacement(int index, ReadOnlySpan<char> text, TokenConversion? conversion, string customRegex)
+        {
+            string newToken = text[index].ToString();
+            int replacementLength = 1;
+
+            if (conversion != null)
             {
-                return string.Empty;
+                newToken = conversion.Get();
+                replacementLength = 0;
+            }
+            else if (IsValidMatch(text[index], customRegex))
+            {
+                string replacementString = GetTokenReplacement(index, text);
+                newToken = Replace(replacementString);
+                replacementLength = replacementString.Length;
             }
 
-            return m_conversions[name].Get();
+            return (newToken, replacementLength);
         }
     }
 }
--- converters/html/HTMLConverter.cs
+++ converters/html/HTMLConverter.cs
@@ -8,8 +8,10 @@ namespace ConvertMarkdownToHTML.html.converters
 {
     public class HTMLConverter : TextConverter
     {
-        private const string SupportConversion = @"[_*#!\n\r]";
-        private Regex m_regex;
+        // Custom Regex patterns
+        private const string NewlinePattern = @"[\n\r]";
+        private const string NewlineReplacementPattern = @"[#]";
+        private const string SupportedConversionsPattern = @"[_*#\n\r]";
 
         /// <summary>
         /// 
@@ -17,16 +19,8 @@ namespace ConvertMarkdownToHTML.html.converters
         public HTMLConverter() :
             base()
         {
-            m_regex = new Regex(SupportConversion);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public override Dictionary<string, TokenConversion> GetConversions()
-        {
-            return new Dictionary<string, TokenConversion>()
+            m_supportedConversions = SupportedConversionsPattern;
+            m_conversions = new Dictionary<string, TokenConversion>()
             {
                 { "#",   new TokenConversion("<h1>", "</h1>") },
                 { "##",  new TokenConversion("<h2>", "</h2>") },
@@ -35,7 +29,8 @@ namespace ConvertMarkdownToHTML.html.converters
                 { "_",   new TokenConversion("<em>", "</em>") },
                 { "*",   new TokenConversion("<em>", "</em>") },
                 { "**",  new TokenConversion("<em>", "</em>") },
-                { "\n",  new TokenConversion("<p>", "</p>") },
+                { "::",  new TokenConversion("<p>", "</p>") },
+                { "$",   new TokenConversion("<p>", "</p>") },
             };
         }
 
@@ -72,46 +67,41 @@ namespace ConvertMarkdownToHTML.html.converters
                 return string.Empty;
             }
 
-            (string, int) startingResult = StartingToken(text, m_conversions["\n"]);
-            stringBuilder.Append(startingResult.Item1);
-            for (int i = startingResult.Item2; i < text.Length; ++i)
+            (string, int) results = GetNewlineReplacement(0, text); ;
+            stringBuilder.Append(results.Item1);
+            for (int i = results.Item2; i < text.Length; ++i)
             {
-                (string, int) results = ReplaceToken(i, text);
+                results = (IsNewline(i, text)) ? GetNewline(i, text) : GetTextReplacement(i, text);
                 if (results.Item2 > 1)
                 {
-                    i += Math.Max(results.Item2 - 1, 0);
+                    i += results.Item2 - 1;
                 }
 
                 stringBuilder.Append(results.Item1);
             }
 
+            stringBuilder.Append(EOL());
             return stringBuilder.ToString();
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="character"></param>
-        /// <returns></returns>
-        private bool IsValidMatch(char character, string customRegex = SupportConversion)
-        {
-            if (customRegex.Equals(SupportConversion))
-            {
-                return m_regex.IsMatch(character.ToString());
-            }
-
-            Regex regex = new Regex(customRegex);
-            return regex.IsMatch(character.ToString());
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
+        /// <param name="index"></param>
         /// <param name="text"></param>
+        /// <param name="isCRLF"></param>
         /// <returns></returns>
-        private (string, int) StartingToken(ReadOnlySpan<char> text, TokenConversion conversion)
+        private (string, int) GetNewline(int index, ReadOnlySpan<char> text)
         {
-            return ReplaceToken(0, text, conversion, @"[#\n\r]");
+            StringBuilder stringBuilder = new StringBuilder();
+            (string, int) results = GetTextReplacement(index, text, m_conversions["$"]);
+            stringBuilder.Append(results.Item1);
+
+            index += 3;
+
+            results = GetNewlineReplacement(index, text);
+            stringBuilder.Append(results.Item1);
+            return (stringBuilder.ToString(), 4);
         }
 
         /// <summary>
@@ -119,25 +109,12 @@ namespace ConvertMarkdownToHTML.html.converters
         /// </summary>
         /// <param name="index"></param>
         /// <param name="text"></param>
+        /// <param name="isCRLF"></param>
         /// <returns></returns>
-        private (string, int) ReplaceToken(int index, ReadOnlySpan<char> text, TokenConversion? conversion, string customRegex)
+        private (string, int) GetNewlineReplacement(int index, ReadOnlySpan<char> text)
         {
-            string newToken = text[index].ToString();
-            int replacementLength = 1;
-
-            if (IsValidMatch(text[index], customRegex))
-            {
-                string replacementString = GetTokenReplacement(index, text);
-                newToken = Replace(replacementString);
-                replacementLength = replacementString.Length;
-            }
-            else if (conversion != null)
-            {
-                newToken = conversion.Get();
-                replacementLength = 0;
-            }
-
-            return (newToken, replacementLength);
+            return (text.Length > index && IsValidMatch(text[index], NewlineReplacementPattern)) ?
+                GetTextReplacement(index, text) : GetTextReplacement(index, text, m_conversions["$"]);
         }
 
         /// <summary>
@@ -145,10 +122,31 @@ namespace ConvertMarkdownToHTML.html.converters
         /// </summary>
         /// <param name="index"></param>
         /// <param name="text"></param>
+        /// <param name="isCRLF"></param>
         /// <returns></returns>
-        private (string, int) ReplaceToken(int index, ReadOnlySpan<char> text, TokenConversion? conversion = null)
+        private bool IsNewline(int index, ReadOnlySpan<char> text, bool isCRLF = true)
         {
-            return ReplaceToken(index, text, conversion, SupportConversion);
+            if (isCRLF)
+            {
+                return (text.Length > index + 3 &&
+                    IsValidMatch(text[index], NewlinePattern) &&
+                    IsValidMatch(text[index + 1], NewlinePattern) &&
+                    IsValidMatch(text[index + 2], NewlinePattern) &&
+                    IsValidMatch(text[index + 3], NewlinePattern));
+            }
+
+            return (text.Length > index + 1 &&
+                IsValidMatch(text[index], NewlinePattern) &&
+                IsValidMatch(text[index + 1], NewlinePattern));
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private string EOL()
+        {
+            return Replace("$");
         }
     }
 }
